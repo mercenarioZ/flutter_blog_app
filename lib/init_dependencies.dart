73bcbdd7d31d05a4a6241c82abdb717b019d2185
mercenarioZ <funnystar320@gmail.com>
2025-07-23 20:43:41 +0700

    fix: add UserSignin to serviceLocator

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,6 +2,7 @@ import 'package:flutter_app/core/secrets/secrets.dart';
 import 'package:flutter_app/features/auth/data/datasources/auth_data_source.dart';
 import 'package:flutter_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:flutter_app/features/auth/domain/repositories/auth_repository.dart';
+import 'package:flutter_app/features/auth/domain/usecases/user_signin.dart';
 import 'package:flutter_app/features/auth/domain/usecases/user_signup.dart';
 import 'package:flutter_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -19,7 +20,7 @@ Future<void> initDependencies() async {
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnonKey,
   );
-  
+
   // Register Supabase client as a lazy singleton. This means it will be created only when it's first requested.
   serviceLocator.registerLazySingleton(() => supabase.client);
 }
@@ -31,7 +32,11 @@ void _initAuth() {
   serviceLocator.registerFactory<AuthRepository>(
     () => AuthRepositoryImpl(serviceLocator()),
   );
+
   serviceLocator.registerFactory(() => UserSignup(serviceLocator()));
+
+  serviceLocator.registerFactory(() => UserSignin(serviceLocator()));
+
   serviceLocator.registerFactory(
     () => AuthBloc(userSignup: serviceLocator(), userSignin: serviceLocator()),
   );
