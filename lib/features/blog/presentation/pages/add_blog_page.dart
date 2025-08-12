e5fa1fb97f922ebe506c15276740f43ef6f86967
mercenarioZ <funnystar320@gmail.com>
2025-08-12 21:01:50 +0700

    feat: create blog input

--- lib/features/blog/presentation/pages/add_blog_page.dart
+++ lib/features/blog/presentation/pages/add_blog_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/core/theme/app_pallete.dart';
 import 'package:flutter_app/core/utils/snackbar.dart';
+import 'package:flutter_app/features/blog/presentation/widgets/blog_editor.dart';
 
 class AddBlogPage extends StatefulWidget {
   static route() =>
@@ -12,6 +13,17 @@ class AddBlogPage extends StatefulWidget {
 }
 
 class _AddBlogPageState extends State<AddBlogPage> {
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+
+  // dispose controllers to avoid memory leaks
+  @override
+  void dispose() {
+    titleController.dispose();
+    contentController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,79 +32,85 @@ class _AddBlogPageState extends State<AddBlogPage> {
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.done_rounded))],
       ),
 
-      // image, title, content
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            // image picker
-            GestureDetector(
-              onTap: () {
-                // Implement image picker logic here
-                snackBar(context, 'Image picker tapped');
-              },
-              child: Container(
-                width: double.infinity,
-                height: 180,
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  border: Border.all(color: Colors.grey.shade400, width: 1.5),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: const Center(
-                  child: Opacity(
-                    opacity: 0.75,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(Icons.add_a_photo, size: 40),
-                        SizedBox(height: 10),
-                        Text('Tap to add image'),
-                      ],
+      // image, title, content. Wrap in SingleChildScrollView to avoid overflow and allow scrolling on smaller screens
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              // image picker
+              GestureDetector(
+                onTap: () {
+                  // Implement image picker logic here
+                  snackBar(context, 'Image picker tapped');
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(color: Colors.grey.shade400, width: 1.5),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Opacity(
+                      opacity: 0.75,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.add_a_photo, size: 40),
+                          SizedBox(height: 10),
+                          Text('Tap to add image'),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // tag selection
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children:
-                    [
-                          'Technology',
-                          'Health',
-                          'Lifestyle',
-                          'Education',
-                          'Programming',
-                          'Productivity',
-                        ]
-                        .map(
-                          (e) => Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 4.0,
-                            ),
-                            child: Chip(
-                              label: Text(e),
-                              side: const BorderSide(
-                                color: AppPallete.borderColor,
+              // tag selection
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children:
+                      [
+                            'Technology',
+                            'Health',
+                            'Lifestyle',
+                            'Education',
+                            'Programming',
+                            'Productivity',
+                          ]
+                          .map(
+                            (e) => Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 4.0,
+                              ),
+                              child: Chip(
+                                label: Text(e),
+                                side: const BorderSide(
+                                  color: AppPallete.borderColor,
+                                ),
                               ),
                             ),
-                          ),
-                        )
-                        .toList(),
+                          )
+                          .toList(),
+                ),
               ),
-            ),
 
-            const SizedBox(height: 20),
-            
-            // title input
+              const SizedBox(height: 20),
 
-          ],
+              // title input
+              BlogEditor(controller: titleController, hintText: 'Title'),
+
+              const SizedBox(height: 20),
+
+              BlogEditor(controller: contentController, hintText: 'Content'),
+            ],
+          ),
         ),
       ),
     );
